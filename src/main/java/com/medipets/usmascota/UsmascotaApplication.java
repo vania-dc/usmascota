594cfa8e93903c075185a2a9b398a24949accff8
VaniaVI <vania.vargas.igor@gmail.com>
2025-12-18 20:51:48 -0300

    inicializacion de proyecto microservicio de mascota

---
+++ src/main/java/com/medipets/usmascota/UsmascotaApplication.java
@@ -0,0 +1,13 @@
+package com.medipets.usmascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsmascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsmascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medipets/usmascota/UsmascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medipets.usmascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsmascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
