98e99b1da79fa7e347a727600106543ce57dbecf
vania-dc <va.vargas@duocuc.cl>
2025-12-19 01:26:24 -0300

    se inicializa repositorio con las carpetas correspondientes

--- src/main/java/com/medipets/usmascota/config/CorsConfig.java
+++ src/main/java/com/medipets/usmascota/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
